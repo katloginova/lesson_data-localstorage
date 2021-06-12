160a0e969f879589e413e1957a0353115ea0c416
Katerina <loginovakaty@gmail.com>
2021-06-12 19:30:06 +0300

    remake code

--- script/script.js
+++ script/script.js
@@ -1,146 +1,137 @@
-'use strict';
+ 'use strict';
 
-const LIST_ITEM_CLASS = 'list__item';
-const DONE_CLASS = 'done';
-const DELETE_BTN_CLASS = 'delete-btn';
-const TASK_ID_ATTRIBUTE_NAME = 'data-task-id';
+ const LIST_ITEM_CLASS = 'list__item';
+ const DONE_CLASS = 'done';
+ const DELETE_BTN_CLASS = 'delete-btn';
+ const TASK_ID_ATTRIBUTE_NAME = 'data-task-id';
 
-const taskTemplate = document.getElementById('taskTemplate').innerHTML;
-const inputTask = document.getElementById('todo');
-const listTasks = document.getElementById('todolist');
+ const taskTemplate = document.getElementById('taskTemplate').innerHTML;
+ const inputTask = document.getElementById('todo');
+ const listTasks = document.getElementById('todolist');
 
-let dataTasks = [];
-
-
-document.getElementById('btnAdd').addEventListener('click', onAddTaskClick);
-listTasks.addEventListener('click', onListTasksClick);
+ let dataTasks = [];
 
 
-init();
-
-function onAddTaskClick() {
-    if (!isEmpty(inputTask.value)) {
-        const formTask = getFormData();
-
-        addNewTask(formTask);
-    }
-
-    resetInput(inputTask);
-    saveToStorage();
-}
-
-function onListTasksClick(e) {
-    const target = e.target;
-    const idTask = getIdTask(target);
-
-    toggleDone(target, idTask);
-    
-    if(target.classList.contains(DONE_CLASS)){
-        changeStateTask(idTask, DONE_CLASS);
-    } else {
-        changeStateTask(idTask, '');
-    }
-
-    if (target.classList.contains(DELETE_BTN_CLASS)) {
-        deleteTaskFromData(idTask);
-        removeTaskFromList(idTask);
-    }
-
-    saveToStorage();
-}
+ document.getElementById('btnAdd').addEventListener('click', onAddTaskClick);
+ listTasks.addEventListener('click', onListTasksClick);
 
 
-/*for onAddTaskClick*/
+ init();
 
-function isEmpty(str) {
-    return str.trim() === '';
-}
+ function onAddTaskClick() {
+     if (!isEmpty(inputTask.value)) {
+         const formTask = getFormData();
 
-function resetInput(str) {
-    str.value = '';
-}
+         addNewTask(formTask);
+     }
 
-function getFormData() {
-    return {
-        id: Date.now(),
-        callTask: inputTask.value,
-        state: ''
-    };
-}
+     resetInput(inputTask);
+ }
 
-function addNewTask(task) {
-    dataTasks.push(task);
-    renderTask(task);
-}
+ function onListTasksClick(e) {
+     const target = e.target;
+     const idTask = getIdTask(target);
 
-function renderTask(task) {
-    const rowTaskHtml = getRowTaskHtml(task);
+     switch (true) {
+         case (target.classList.contains(LIST_ITEM_CLASS)):
+             toggleDone(target);
+             changeStateTask(idTask);
+             break;
+         case (target.classList.contains(DELETE_BTN_CLASS)):
+             deleteTaskFromData(idTask);
+             removeTaskFromList(idTask);
+             break;
+     }
 
-    listTasks.insertAdjacentHTML('beforeend', rowTaskHtml);
-}
-
-function getRowTaskHtml(task) {
-    return taskTemplate
-        .replace('{{id}}', task.id)
-        .replace('{{classDone}}', task.state)
-        .replace('{{task}}', task.callTask);
-}
+ }
 
 
-/*for onListTasksClick*/
+ /*for onAddTaskClick*/
 
-function toggleDone(elem) {
-    if(elem.classList.contains(LIST_ITEM_CLASS)){
-        elem.classList.toggle(DONE_CLASS);
-    }
-}
+ function isEmpty(str) {
+     return str.trim() === '';
+ }
 
-function changeStateTask(id, classState) {
-    dataTasks.forEach((task) => {
-        if (task.id === id) {
-            return (task.state = classState);
-        }
-    });
+ function resetInput(str) {
+     str.value = '';
+ }
 
-    return dataTasks;
-}
+ function getFormData() {
+     return {
+         id: Date.now(),
+         callTask: inputTask.value,
+         isDone: false
+     };
+ }
 
-function deleteTaskFromData(id) {
-    dataTasks = dataTasks.filter((item) => item.id !== id);
-}
+ function addNewTask(task) {
+     dataTasks.push(task);
+     renderTask(task);
+     saveToStorage();
+ }
 
-function removeTaskFromList(id) {
-    const taskElement = getTaskElement(id);
-    taskElement.remove();
-}
+ function renderTask(task) {
+     const rowTaskHtml = getRowTaskHtml(task);
 
-function getIdTask(elem) {
-    const row = elem.closest('.' + LIST_ITEM_CLASS);
-    return +row.dataset.taskId;
-}
+     listTasks.insertAdjacentHTML('beforeend', rowTaskHtml);
+ }
 
-function getTaskElement(id) {
-    return listTasks.querySelector(`[${TASK_ID_ATTRIBUTE_NAME}="${id}"]`);
-}
+ function getRowTaskHtml(task) {
+     return taskTemplate
+         .replace('{{id}}', task.id)
+         .replace('{{classDone}}', task.isDone ? DONE_CLASS : '')
+         .replace('{{task}}', task.callTask);
+ }
 
 
-/*for localStorage */
+ /*for onListTasksClick*/
 
-function saveToStorage() {
-    localStorage.setItem('listTasks', JSON.stringify(dataTasks));
-}
+ function getIdTask(elem) {
+     const row = elem.closest('.' + LIST_ITEM_CLASS);
+     return +row.dataset.taskId;
+ }
 
-function restoreFromStorage() {
-    return JSON.parse(localStorage.getItem('listTasks'));
-}
+ function toggleDone(elem) {
+     elem.classList.toggle(DONE_CLASS);
+ }
 
-function init() {
-    dataTasks = restoreFromStorage();
+ function changeStateTask(id) {
+     dataTasks = dataTasks.map((task) =>
+         task.id !== id ?
+         task : {
+             ...task,
+             isDone: !task.isDone
+         });
 
-    if (dataTasks !== null) {
-        dataTasks.forEach((task) => renderTask(task));
-    } else {
-        dataTasks = [];
-    }
-}
+     saveToStorage();
+ }
 
+ function deleteTaskFromData(id) {
+     dataTasks = dataTasks.filter((item) => item.id !== id);
+     saveToStorage();
+ }
+
+ function removeTaskFromList(id) {
+     listTasks.querySelector(`[${TASK_ID_ATTRIBUTE_NAME}="${id}"]`).remove();
+ }
+
+
+ /*for localStorage */
+
+ function saveToStorage() {
+     localStorage.setItem('listTasks', JSON.stringify(dataTasks));
+ }
+
+ function restoreFromStorage() {
+     return JSON.parse(localStorage.getItem('listTasks'));
+ }
+
+ function init() {
+     dataTasks = restoreFromStorage();
+
+     if (dataTasks !== null) {
+         dataTasks.forEach((task) => renderTask(task));
+     } else {
+         dataTasks = [];
+     }
+ }
